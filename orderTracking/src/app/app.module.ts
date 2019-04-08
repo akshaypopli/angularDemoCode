6d32ee1154c326ea224546c43c03a6a6638ddfd7
Mac <mac@Macs-MacBook-Air.local>
2019-04-09 00:13:26 +0530

    implement angular router-outlet

--- orderTracking/src/app/app.module.ts
+++ orderTracking/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms'; 
+const appRoutes=[];
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule   
   ],
   providers: [],
   bootstrap: [AppComponent]
