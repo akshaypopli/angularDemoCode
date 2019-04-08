302de71dc76d7415f2f135083a71d8d6cc94ac61
Mac <mac@Macs-MacBook-Air.local>
2019-04-09 00:29:37 +0530

    creation of status bar component

--- orderTracking/src/app/app.module.ts
+++ orderTracking/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component'; 
 const appRoutes=[];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusBarComponent,
+    HeaderBarComponent
   ],
   imports: [
     BrowserModule,
